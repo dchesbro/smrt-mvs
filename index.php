33dacf21f0b8cb6d65833424006581fa706cf4e9
Dwight Chesbro <dwight.chesbro@simplymusic.com>
2017-07-24 13:16:16 -0700

    More style changes.

--- index.php
+++ index.php
@@ -349,6 +349,7 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" crossorigin="anonymous">
 		
 		<!-- Web fonts -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" crossorigin="anonymous">
 		<script src="//use.edgefonts.net/geo.js"></script>
 		
 		<!-- Custom styles -->
